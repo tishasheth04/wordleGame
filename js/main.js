1c8b79c7625323e19d0ffb664fc2c826aa42a4c1
Tisha <shethtisha@gmail.com>
2024-11-20 16:06:24 -0500

    Made the game more playable- can check words

--- js/main.js
+++ js/main.js
@@ -1,6 +1,42 @@
 document.addEventListener("DOMContentLoaded",() =>{
     createSquares();
+    let guessedWords = [[]];
+    let availableSpace = 1;
+    let word = "dairy";
     
+    const keys = document.querySelectorAll(".keyboard-row button");
+
+    function getCurrentWordArr(){
+        const numberOfGuessedWords = guessedWords.length;
+        return guessedWords[numberOfGuessedWords - 1];
+    }
+
+    function updateGuessedWords(letter){
+        const currentWordArr = getCurrentWordArr();
+
+        if(currentWordArr && currentWordArr.length < 5){
+            currentWordArr.push(letter);
+
+            const availableSpaceEl = document.getElementById(String(availableSpace));
+            availableSpace = availableSpace +1;
+
+            availableSpaceEl.textContent = letter;
+        }
+    }
+
+    function handleSubmitWord(){
+        const currentWordArr = getCurrentWordArr();
+        if(currentWordArr.length !== 5) {
+            window.alert("Word must be 5 letters");
+        }
+
+        const currentWord = currentWordArr.join('')
+
+        if(currentWord == word){
+            window.alert("YAYYA! Congrats!");
+        }
+    }
+
     function createSquares(){
         const gameBoard = document.getElementById("board")
 
@@ -11,4 +47,17 @@ document.addEventListener("DOMContentLoaded",() =>{
             gameBoard.appendChild(square);
         }
     }
-})
+
+    for(let i = 0; i < keys.length; i++){
+        keys[i].onclick = ({target}) =>{
+            const letter = target.getAttribute("data-key");
+            if(letter == 'enter'){
+                handleSubmitWord();
+                return;
+            }
+            updateGuessedWords(letter);
+        }
+    }
+
+
+});
