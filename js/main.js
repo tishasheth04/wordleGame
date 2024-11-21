f753fd394dfc344a8c8434f9406d3647e9543637
Tisha <shethtisha@gmail.com>
2024-11-21 12:56:26 -0500

    Working wordle, just need to unhardcode the word

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded",() =>{
     let guessedWords = [[]];
     let availableSpace = 1;
     let word = "dairy";
+    let guessedWordCount = 0;
     
     const keys = document.querySelectorAll(".keyboard-row button");
 
@@ -24,17 +25,55 @@ document.addEventListener("DOMContentLoaded",() =>{
         }
     }
 
+    function getTileColor(letter, index){
+        const isCorrectLetter = word.includes(letter);
+
+        if(!isCorrectLetter){
+            return "rgb(58,58,60)";
+        }
+
+        const letterInThatPosition = word.charAt(index);
+        
+        const isCorrectPosition = letter === letterInThatPosition;
+        
+        if(isCorrectPosition){
+            return "rgb(83,141,78)";
+        }
+
+        return "rgb(181, 159, 59)";
+    }
+
     function handleSubmitWord(){
         const currentWordArr = getCurrentWordArr();
         if(currentWordArr.length !== 5) {
             window.alert("Word must be 5 letters");
         }
 
-        const currentWord = currentWordArr.join('')
+        const currentWord = currentWordArr.join("");
+        const firstLetterId = guessedWordCount * 5 + 1;
+
+
+        const interval = 200;
+        currentWordArr.forEach((letter,index) => {
+            setTimeout(() =>{
+                const tileColor = getTileColor(letter, index);
+                const letterId = firstLetterId + index;
+                const letterEl = document.getElementById(letterId);
+                letterEl.classList.add("animate__flipInX");
+                letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
+            }, interval * index);
+        });
+
+        guessedWordCount += 1;
 
         if(currentWord == word){
             window.alert("YAYYA! Congrats!");
         }
+        if(guessedWords.length === 6){
+            window.alert(`Sorry no more guesses! The word is: ${word}.`);
+        }
+
+        guessedWords.push([]);
     }
 
     function createSquares(){
@@ -43,6 +82,7 @@ document.addEventListener("DOMContentLoaded",() =>{
         for(let index = 0; index < 30; index++){
             let square = document.createElement("div");
             square.classList.add("square");
+            square.classList.add("animate__animated");
             square.setAttribute("id", index+1);
             gameBoard.appendChild(square);
         }
